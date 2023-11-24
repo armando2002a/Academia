39b66886a13d533b048be721a084cdb3ac63ff93
HP <HP@ARMANDOABURTO>
2023-11-24 15:03:21 -0600

    Se agrega nuevos cambios

--- Datos/DataDb/AppDbContext.cs
+++ Datos/DataDb/AppDbContext.cs
@@ -13,5 +13,12 @@ namespace Datos.DataDb
         public DbSet<Estudiante> Estudiante { get; set; } 
         public DbSet<Docente> Docente { get; set; }
         public DbSet<Colaborador> Colaborador { get; set; }
+        public DbSet<Asignatura> Asignatura { get; set; }
+        public DbSet<Horario> Horario { get; set; }
+        public DbSet<Aula> Aula { get; set; }
+        public DbSet<Matricula> Matricula { get; set; }
+        public DbSet<Nota> Nota { get; set; }
+
+
     }
 }
