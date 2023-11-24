1b416af981a42e6b5554bc211fc3ecf0a407c4e8
armando2002a <91574504+armando2002a@users.noreply.github.com>
2023-11-24 16:00:43 -0600

    Update DocenteServicio.cs

--- Servicio/Docente/DocenteServicio.cs
+++ Servicio/Docente/DocenteServicio.cs
@@ -11,9 +11,12 @@ namespace Servicio.Docente
             _docenteRepositorio = docenteRepositorio;
         }
 
-        public List<Datos.Models.Docente> ListaDocente()
-        {
-            return _docenteRepositorio.ListaDocente();
-        }
+        public List<Datos.Models.Docente> ListaDocente() => _docenteRepositorio.ListaDocente();
+
+        public List<Datos.Models.Docente> ListaDocentePorID(int DocenteID) => _docenteRepositorio.ListaDocentePorID(DocenteID);
+
+        public bool GuardarDocente(Datos.Models.Docente Docente) => _docenteRepositorio.GuardarDocente(Docente);
+
+        public bool ActualizarDocente(Datos.Models.Docente Docente) => _docenteRepositorio.ActualizarDocente(Docente);
     }
 }
