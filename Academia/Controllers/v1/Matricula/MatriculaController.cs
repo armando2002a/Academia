fc0b90c8e7f497ab31445828a18a8ca4a771b95b
armando2002a <91574504+armando2002a@users.noreply.github.com>
2023-11-24 16:14:11 -0600

    Update MatriculaController.cs

--- Academia/Controllers/v1/Matricula/MatriculaController.cs
+++ Academia/Controllers/v1/Matricula/MatriculaController.cs
@@ -1,6 +1,43 @@
 ﻿namespace Academia.Controllers.v1.Matricula
 {
-    public class MatriculaController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatriculaController : ControllerBase
     {
+        private readonly IMatriculaServicio _matriculaServicio;
+
+        public MatriculaController(IMatriculaServicio matriculaServicio)
+        {
+            _matriculaServicio = matriculaServicio;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public IActionResult Lista()
+        {
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "Ok", response = _matriculaServicio.ListaMatricula() });
+           
+        }
+
+        [HttpPost]
+        [Route("ListaPorID")]
+        public IActionResult ListaPorID([FromBody] Datos.Models.Matricula Matricula)
+        {
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "Ok", response = _matriculaServicio.ListaMatriculaPorID(Matricula.MatriculaID) });
+        }
+
+        [HttpPost]
+        [Route("Guardar")]
+        public IActionResult Guardar([FromBody] Datos.Models.Matricula Matricula)
+        {
+            return Ok(_matriculaServicio.GuardarMatricula(Matricula));
+        }
+
+        [HttpPost]
+        [Route("Actualizar")]
+        public IActionResult Actualizar([FromBody] Datos.Models.Matricula Matricula)
+        {
+            return Ok(_matriculaServicio.ActualizarMatricula(Matricula));
+        }
     }
 }
