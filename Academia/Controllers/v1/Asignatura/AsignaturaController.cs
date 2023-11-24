538a7da73bc32ded4a6d257032acf26164ef045c
armando2002a <91574504+armando2002a@users.noreply.github.com>
2023-11-24 15:29:54 -0600

    Update AsignaturaController.cs

--- Academia/Controllers/v1/Asignatura/AsignaturaController.cs
+++ Academia/Controllers/v1/Asignatura/AsignaturaController.cs
@@ -1,6 +1,43 @@
 ﻿namespace Academia.Controllers.v1.Asignatura
 {
-    public class AsignaturaController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsignaturaController : ControllerBase
     {
+        private readonly IAsignaturaServicio _asignaturaServicio;
+
+        public AsignaturaController(IAsignaturaServicio asignaturaServicio)
+        {
+            _asignaturaServicio = asignaturaServicio;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public IActionResult Lista()
+        {
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "Ok", response = _asignaturaServicio.ListaAsignatura() });
+           
+        }
+
+        [HttpPost]
+        [Route("ListaPorID")]
+        public IActionResult ListaPorID([FromBody] Datos.Models.Asignatura Asignatura)
+        {
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "Ok", response = _asignaturaServicio.ListaAsignaturaPorID(Asignatura.AsignaturaID) });
+        }
+
+        [HttpPost]
+        [Route("Guardar")]
+        public IActionResult Guardar([FromBody] Datos.Models.Asignatura Asignatura)
+        {
+            return Ok(_asignaturaServicio.GuardarAsignatura(Asignatura));
+        }
+
+        [HttpPost]
+        [Route("Actualizar")]
+        public IActionResult Actualizar([FromBody] Datos.Models.Asignatura Asignatura)
+        {
+            return Ok(_asignaturaServicio.ActualizarAsignatura(Asignatura));
+        }
     }
 }
