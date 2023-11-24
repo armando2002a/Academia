56b882bb5be78ed6a6804bd213227ff6dc0574c8
armando2002a <91574504+armando2002a@users.noreply.github.com>
2023-11-24 15:14:18 -0600

    Update IAsignaturaServicio.cs

--- Servicio/Asignatura/IAsignaturaServicio.cs
+++ Servicio/Asignatura/IAsignaturaServicio.cs
@@ -2,5 +2,9 @@
 {
     public interface IAsignaturaServicio
     {
+        List<Datos.Models.Asignatura> ListaAsignatura();
+        List<Datos.Models.Asignatura> ListaAsignaturaPorID(int AsignaturaID);
+        bool GuardarAsignatura(Datos.Models.Asignatura Asignatura);
+        bool ActualizarAsignatura(Datos.Models.Asignatura Asignatura);
     }
 }
