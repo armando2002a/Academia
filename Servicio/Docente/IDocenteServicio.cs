fc7a89194d37e165dd01eaae1d404a5efb8ad1ea
armando2002a <91574504+armando2002a@users.noreply.github.com>
2023-11-24 15:52:11 -0600

    Update IDocenteServicio.cs

--- Servicio/Docente/IDocenteServicio.cs
+++ Servicio/Docente/IDocenteServicio.cs
@@ -3,5 +3,8 @@
     public interface IDocenteServicio
     {
         List<Datos.Models.Docente> ListaDocente();
+        List<Datos.Models.Docente> ListaDocentePorID(int DocenteID);
+        bool GuardarDocente(Datos.Models.Docente Docente);
+        bool ActualizarDocente(Datos.Models.Docente Docente);
     }
 }
