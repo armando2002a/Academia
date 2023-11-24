263a53a32d7d3f83df610da01fd5a0683af96ecf
armando2002a <91574504+armando2002a@users.noreply.github.com>
2023-11-24 15:21:53 -0600

    Update AsignaturaServicio.cs

--- Servicio/Asignatura/AsignaturaServicio.cs
+++ Servicio/Asignatura/AsignaturaServicio.cs
@@ -1,6 +1,20 @@
 ﻿namespace Servicio.Asignatura
 {
-    internal class AsignaturaServicio
+    public class AsignaturaServicio : IAsignaturaServicio
     {
+        private readonly IAsignaturaRepositorio _asignaturaRepositorio;
+
+        public EstudianteServicio(IAsignaturaRepositorio asignaturaRepositorio)
+        {
+            _asignaturaRepositorio = asignaturaRepositorio;
+        }
+
+        public List<Datos.Models.Asignatura> ListaAsignatura() => _asignaturaRepositorio.ListaAsignatura();
+
+        public List<Datos.Models.Asignatura> ListaAsignaturaPorID(int AsignaturaID) => _asignaturaRepositorio.ListaAsignaturaPorID(AsignaturaID);
+
+        public bool GuardarAsignatura(Datos.Models.Asignatura Asignatura) => _asignaturaRepositorio.GuardarAsignatura(Asignatura);
+
+        public bool ActualizarAsignatura(Datos.Models.Asignatura Asignatura) => _asignaturaRepositorio.ActualizarAsignatura(Asignatura);
     }
 }
