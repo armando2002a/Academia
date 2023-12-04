153feba9b16aec732e0d57e9686b491791616e51
armando2002a <91574504+armando2002a@users.noreply.github.com>
2023-12-04 14:21:22 -0600

    Se agrega Horario  Matricula Datos

---
+++ Datos/Models/HorarioMatriculaDatos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Datos.Models
+{
+    public class InformacionDetalladaClases
+    {
+        [Key]
+        public int? HorarioID { get; set; }
+        public string? NombreDocente { get; set; }
+        public string? AsignaturaNombre { get; set; }
+        public int? Grado { get; set; }
+        public int? Nivel { get; set; }
+		public int? Numero_de_Aula { get; set; }
+        public string? Hora_Clase { get; set; }
+    }
+}
