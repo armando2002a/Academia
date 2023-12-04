960a93f48ad3b04b6a044181b6213e53cc002663
armando2002a <91574504+armando2002a@users.noreply.github.com>
2023-12-04 16:43:23 -0600

    Se actualizo IHorarioServicio.cs

--- Servicio/Horario/IHorarioServicio.cs
+++ Servicio/Horario/IHorarioServicio.cs
@@ -3,6 +3,7 @@
     public interface IHorarioServicio 
     {
         List<Datos.Models.Horario> ListaHorario();
+        List<Datos.Models.HorarioMatricula> ListaHorarioMatricula();
         List<Datos.Models.Horario> ListaHorarioPorID(int HorarioID);
         bool GuardarHorario(Datos.Models.Horario Horario);
         bool ActualizarHorario(Datos.Models.Horario Horario);
